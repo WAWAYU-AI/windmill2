34c03ceb9cd45dad5957d00209a86699a31aab47
ovalene <894381852@qq.com>
2025-03-02 23:35:17 +0800

    2025/3/2

--- armor/src/AimAuto.cpp
+++ armor/src/AimAuto.cpp
@@ -225,7 +225,7 @@ void AimAuto::auto_aim(cv::Mat &src, Translator &ts, double dt)
         }
         if (ts.message.crc) cnt ++;
         else cnt = 0;
-        if (cnt < 10) ts.message.crc = 0;
+        if (cnt < gp->max_lost_frame) ts.message.crc = 0;
     #ifdef DEBUGMODE
         if (ts.message.crc){
         cv::putText(src, "latency: " + std::to_string(ts.message.latency), cv::Point(1050, 150), cv::FONT_HERSHEY_PLAIN, 2, cv::Scalar(0, 255, 0), 1);
